fdc6d68b16a87f52230800be377168dc0f450124
Matias Agustin Area <40324614+MatiArea@users.noreply.github.com>
2021-01-13 23:29:50 -0300

    .

--- src/index.js
+++ src/index.js
@@ -3,10 +3,10 @@ import "@babel/polyfill";
 import sequelize from "./BaseDeDatos/database";
 
 async function main() {
-  var server_port = process.env.YOUR_PORT || process.env.PORT || 80;
-  var server_host = process.env.YOUR_HOST || "0.0.0.0";
+  var port = process.env.PORT || 80;
+  var host = process.env.HOST || "0.0.0.0";
   try {
-    await app.listen(server_port, server_host, function () {
+    await app.listen(port, host, function () {
       console.log("Listening on port %d", server_port);
     });
     await sequelize
